cdc3d6e65c1d8bb3716fe3cc88878369ef1a5f5d
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-30 11:24:25 +0500

    Task6

---
+++ Tyuiu.MkhitaryanSG.Sprint3.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.MkhitaryanSG.Sprint3.Task6.V27.Lib
+{
+    public class DataService : ISprint3Task6V27
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x = 0;
+            int sum = 0;
+            for (x = startValue; x < stopValue; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        if (d > 9)
+                        {
+                            sum = sum + d;
+                        }
+                    }
+                }
+
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ Tyuiu.MkhitaryanSG.Sprint3.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint3.Task6.V27.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint3.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void GetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 14;
+            int stopValue = 20;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 99;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
